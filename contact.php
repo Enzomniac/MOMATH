4920c40a8cc5e39f81a8df3da45f98fa9ff2b84b
Richard Upton <38717726+Enzomniac@users.noreply.github.com>
2020-05-03 17:16:44 +0200

    contact logic

--- contact.php
+++ contact.php
@@ -1,9 +1,12 @@
 <?php
 include("header.php");
+$messageControl = checkMessagePost();
 ?>
     <main>
         <section class="full-width">
-            <div class="message-notification">NOTIFICATION AREA</div>
+            <div class="message-notification" style="display: <?php echo($messageControl['firstVisit']); ?>">
+                <?php echo($messageControl['htmlMessage']); ?>
+            </div>
             <div class="contact">
                 <div class="contact-details">
                     <div class="a-detail">
@@ -26,13 +29,17 @@ include("header.php");
                 <form action="contact.php" class="form" method="post">
                     <div class="form__row">
                         <label for="name" class="label__name">Name:</label>
-                        <input type="text" class="input__name" placeholder="YOUR NAME" name="aName">
+                        <input type="text" class="input__name" placeholder="YOUR NAME" name="aName" value="<?php echo($messageControl['name']); ?>">
                     </div>
                     <div class="form__row">
                         <label for="email" class="label__email">Email:</label>
-                        <input type="text" class="input__email" placeholder="YOUR EMAIL" name="email">
+                        <input type="text" class="input__email" placeholder="YOUR EMAIL" name="email" value="<?php echo($messageControl['email']); ?>">
                     </div>
-                    <textarea name="message" id="" cols="30" rows="10" class="input__text-area" placeholder="YOUR MESSAGE HERE"></textarea>
+                    <textarea name="message" id="" cols="30" rows="10" class="input__text-area" placeholder="YOUR MESSAGE HERE"><?php echo($messageControl['message']); ?></textarea>
+                    <div class="form__row">
+                        <label for="human" class="label__human" value="<?php echo($messageControl['email']); ?>">5 + 7 = </label>
+                        <input type="text" class="input__human" placeholder="?" name="human">
+                    </div>
                     <button class="button">
                         Send Message
                     </button>
--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
-foreach ($_POST as $key => $value) {
+/* foreach ($_POST as $key => $value) {
     echo($key . " => " . $value);
     echo("<br>");
-}
+} */
     
 function checkActive($checkThis) {    
     $currentUrl = $_SERVER['PHP_SELF'];    
@@ -11,5 +11,46 @@ function checkActive($checkThis) {
     if ($currentUrl == $checkThis) {
         echo("active");
     }   
+}
+
+function checkMessagePost() {
+    $messageControl = array(
+        'firstVisit' => 'block',
+        'htmlMessage' => '',
+        'name' => '',
+        'email' => '',
+        'message' => '',
+        'human' => ''     
+    );
+    if (!$_POST) {
+        echo("First Vist");
+        $messageControl['firstVisit'] = "none";
+    }   elseif ($_POST['aName'] && $_POST['email'] && $_POST['message'] && ($_POST['human'] == strval(12)) ) {
+        $messageControl['htmlMessage'] .= 'Message sent succesfully, I will respond as soon as possible';
+    }   else    {
+        if (!$_POST['aName']) {
+            $messageControl['htmlMessage'] .= "Please enter your name!<br>";
+        }   else {
+            $messageControl['name'] = $_POST['aName'];
+        }      
+        if (!$_POST['email']) {
+            $messageControl['htmlMessage'] .= "Please enter your email!<br>";
+        }   else {
+            $messageControl['email'] = $_POST['email'];
+        }   
+        if (!$_POST['message']) {
+            $messageControl['htmlMessage'] .= "Please enter a message!<br>";
+        }   else {
+            $messageControl['message'] = $_POST['message'];
+        }
+        if (!$_POST['human'] || $_POST['human'] != strval(12)) {
+            $messageControl['htmlMessage'] .= "Check your math human!";
+        } else {
+            $messageControl['human'] = $_POST['human'];
+        }   
+    }   
+    
+    
+    return $messageControl;
 }
 ?>
