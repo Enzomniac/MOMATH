0f221d91368ad6fa7012945853e326b5437462fc
Richard Upton <38717726+Enzomniac@users.noreply.github.com>
2020-06-07 18:01:58 +0200

    base dev

--- learn.php
+++ learn.php
@@ -2,25 +2,7 @@
 include("header.php");
 ?>
     <main>
-        <section class="full-width">
-            <h2 class="section-title">
-               The Catch-Up Series (FREE)
-            </h2>
-            <p class="section-copy">
-                Quick and dirty summary and review series for each mathematical topic by grade.
-            </p>
-        </section>
-        <section class="of-cards">
-            <div class="a-card">
-                <h3>GRADE 10</h3>
-            </div>
-            <div class="a-card">
-                <h3>GRADE 11</h3>
-            </div>
-            <div class="a-card">
-                <h3>GRADE 12</h3>
-            </div>
-        </section>
+
     </main>
 
 <?php
